744b474f0dbbf4ed03216ce7981a5c889ed1d5d8
KunalModi117 <kunalmodi117@gmail.com>
2023-07-05 22:45:44 +0530

    added 3 pages and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ProductListingPage from './components/productlistingpage';
+import ProductDetailPage from './components/productdetailpage';
+import CartPage from './components/cartpage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to see the magic.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/products' Component={ProductListingPage} />
+          <Route path='/products/detail' Component={ProductDetailPage} />
+          <Route path='/cart' Component={CartPage} />
+          {/* <ProductListingPage></ProductListingPage> */}
+          {/* <ProductDetailPage></ProductDetailPage> */}
+          {/* <CartPage></CartPage> */}
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/components/productdetailpage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function ProductDetailPage() {
+    return (
+        <h1>ProductDetailPage</h1>
+    );
+}
